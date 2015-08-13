55a4fb5874810587eaeba47ba36f3a9f405dce86
Fernando Gavilan <fernandogavilan@gmail.com>
2015-08-13 20:21:41 +0200

    game completed, edition in progress

--- src/Stampi/AdminBundle/Form/EditionType.php
+++ src/Stampi/AdminBundle/Form/EditionType.php
@@ -15,7 +15,18 @@ class EditionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('publishDate')
+            ->add('publishDate', 'date', array(
+                    'attr' => array('class' => 'datepicker')
+                )
+            )
+            ->add('textI18n', 'collection', array(
+                    'type' => new textI18nType(),
+                    'allow_add' => true,
+                    'allow_delete' => true,
+                    'prototype' => true,
+                    'by_reference' => false
+                )
+            )
             ->add('symbol')
         ;
     }
